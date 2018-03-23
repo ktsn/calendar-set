5e65faf8a96f832734edb616316ff321609b85f8
ktsn <ktsn55@gmail.com>
2018-03-23 17:20:11 +0900

    chore: add format util

--- src/utils.js
+++ src/utils.js
@@ -13,3 +13,18 @@ export function accumlateWhile(fn, condition) {
   }
   return res
 }
+
+export function format(str, values) {
+  const placeholderRE = /\{(\d+)\}/g
+  const escapedRE = /\\\{(\d+)\\\}/g
+
+  return str
+    .replace(placeholderRE, (_, index) => {
+      const i = Number(index)
+      const value = values[i] || ''
+      return value
+    })
+    .replace(escapedRE, (_, index) => {
+      return '{' + index + '}'
+    })
+}
---
+++ test/format.spec.js
@@ -0,0 +1,23 @@
+import { format } from '../src/utils'
+
+describe('format util', () => {
+  it('should output the input string as is', () => {
+    const actual = format('test value')
+    expect(actual).toBe('test value')
+  })
+
+  it('should replace placeholders', () => {
+    const actual = format('{0} foo {1} bar', ['123', '456'])
+    expect(actual).toBe('123 foo 456 bar')
+  })
+
+  it('should handle escaped { and }', () => {
+    const actual = format('\\{0\\} {0}', ['test'])
+    expect(actual).toBe('{0} test')
+  })
+
+  it('should replace with empty value with null or undefined', () => {
+    const actual = format('{0} {1} {2}', [null, undefined, 'test'])
+    expect(actual).toBe('  test')
+  })
+})
