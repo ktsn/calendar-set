64e627c0b5fe8344f30ec367f9cd90c3dbe9f324
ktsn <ktsn55@gmail.com>
2018-03-29 15:30:39 +0900

    feat: allow to highlight data

--- examples/DateCalendar/main.js
+++ examples/DateCalendar/main.js
@@ -1,15 +1,5 @@
-import { en, DateCalendar } from '../../src'
+import App from './App.html'
 
-const normal = new DateCalendar({
-  target: document.getElementById('normal'),
-  data: {
-    locales: en,
-    selected: new Date()
-  }
-})
-
-normal.on('select', selected => {
-  normal.set({
-    selected
-  })
+new App({
+  target: document.getElementById('app')
 })
